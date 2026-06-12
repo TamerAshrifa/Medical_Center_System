27621d253c25f967e2e231131c8604a8969a7e85
TamerAshrifa <tamrashryft@gmail.com>
2026-06-12 20:33:42 +0300

    Fixing Validation Error Message

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -113,7 +113,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->render(function (ValidationException $e, $request) {
             return response()->json([
                 'result' => 'Fail',
-                'message' => 'Validation failed',
+                'message' => 'Invalid input',
                 'errors' => $e->errors(),
             ], 422);
         });
