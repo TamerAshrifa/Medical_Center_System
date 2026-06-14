5c7190496c1642651d7210876f3f1366083a7b81
TamerAshrifa <tamrashryft@gmail.com>
2026-06-15 00:41:04 +0300

    Fixing deleting APIs2

--- routes/api.php
+++ routes/api.php
@@ -59,10 +59,10 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
 
-    // Schedules APIs
-    Route::prefix('schedules')->group(function () {
-        Route::post('', [WorkScheduleController::class, 'store'])->middleware('CheckDoctor');
-    });
+    // // Schedules APIs
+    // Route::prefix('schedules')->group(function () {
+    //     Route::post('', [WorkScheduleController::class, 'store'])->middleware('CheckDoctor');
+    // });
 });
 // User 1 (Admin) token: 1|vFlKneyH4iLgFycMTjqR3pUQtyjy5LAOxwKAakYDa02cf404
 // User 26 (Doctor 11) token: 
