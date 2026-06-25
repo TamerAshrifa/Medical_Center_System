eec547945a45e27c07a7fc8309a53ee5da81a018
TamerAshrifa <tamrashryft@gmail.com>
2026-06-25 13:23:21 +0300

    Fixing 'Interface Binding' bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,8 +5,11 @@ namespace App\Providers;
 use App\Models\WorkSchedule;
 use App\Repositories\AppointmentRepository;
 use App\Repositories\DoctorRepository;
+use App\Repositories\DoctorSpecialityRepository;
 use App\Repositories\Interfaces\AppointmentRepositoryInterface;
 use App\Repositories\Interfaces\DoctorRepositoryInterface;
+use App\Repositories\Interfaces\DoctorSpecialityRepositoryInterface;
+use App\Repositories\Interfaces\MedicalRecordAccessRepositoryInterface;
 use App\Repositories\Interfaces\PatientRepositoryInterface;
 use App\Repositories\Interfaces\RepositoryInterface;
 use App\Repositories\Interfaces\ResetPasswordTokenRepositoryInterface;
@@ -15,6 +18,7 @@ use App\Repositories\Interfaces\SchedulingRepositoryInterface;
 use App\Repositories\Interfaces\SpecialityRepositoryInterface;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 use App\Repositories\Interfaces\VisitRepositoryInterface;
+use App\Repositories\MedicalRecordAccessRepository;
 use App\Repositories\PatientRepository;
 use App\Repositories\Repository;
 use App\Repositories\ResetPasswordTokenRepository;
@@ -34,15 +38,21 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(
-            UserRepositoryInterface::class,
-            UserRepository::class
+            AppointmentRepositoryInterface::class,
+            AppointmentRepository::class
         );
-
         $this->app->bind(
-            ResetPasswordTokenRepositoryInterface::class,
-            ResetPasswordTokenRepository::class
+            DoctorRepositoryInterface::class,
+            DoctorRepository::class
         );
-
+        $this->app->bind(
+            DoctorSpecialityRepositoryInterface::class,
+            DoctorSpecialityRepository::class
+        );
+        $this->app->bind(
+            MedicalRecordAccessRepositoryInterface::class,
+            MedicalRecordAccessRepository::class
+        );
         $this->app->bind(
             PatientRepositoryInterface::class,
             PatientRepository::class
@@ -52,24 +62,24 @@ class AppServiceProvider extends ServiceProvider
             Repository::class
         );
         $this->app->bind(
+            ResetPasswordTokenRepositoryInterface::class,
+            ResetPasswordTokenRepository::class
+        );
+        $this->app->bind(
             RoomRepositoryInterface::class,
             RoomRepository::class
-        );
-        $this->app->bind(
-            DoctorRepositoryInterface::class,
-            DoctorRepository::class
-        );
-        $this->app->bind(
-            SpecialityRepositoryInterface::class,
-            SpecialityRepository::class
         );
         $this->app->bind(
             SchedulingRepositoryInterface::class,
             SchedulingRepository::class
         );
         $this->app->bind(
-            AppointmentRepositoryInterface::class,
-            AppointmentRepository::class
+            SpecialityRepositoryInterface::class,
+            SpecialityRepository::class
+        );
+        $this->app->bind(
+            UserRepositoryInterface::class,
+            UserRepository::class
         );
         $this->app->bind(
             VisitRepositoryInterface::class,
